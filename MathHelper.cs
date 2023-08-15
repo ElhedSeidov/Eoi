c9012252aa0a77191912db9d352ba7fcdf2d58ea
reshadsaidov <alhadss@code.edu.az>
2023-08-16 02:11:22 +0400

    first commit

---
+++ MathHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMath
+{
+    public class MathHelper
+    {
+        public int PrintValue(int a, int b)
+        { return a + b;}  
+        public int PrintValue(bool a)
+        {return a? 1 : 0;}  
+        public void PrintValue(string a, string b)
+        { Console.WriteLine(a + b); }
+        public int PrintValue(int a, int b, int c)
+        { return a * b * c;}
+    }
+
+}    
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleAppMath
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MathHelper mathHelper = new MathHelper();
+            Console.WriteLine(mathHelper.PrintValue(2,5));   
+            Console.WriteLine(mathHelper.PrintValue(true));
+            mathHelper.PrintValue("wer", "erw");
+            Console.WriteLine(mathHelper.PrintValue(2, 5, 7));
+        }   
+    }
+}
